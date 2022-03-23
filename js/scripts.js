dc71e032e2a836e16ce05e3ea9f969314b19185f
masalamind <themasalacollective@gmail.com>
2022-03-23 20:02:02 +0300

    Added new stuff

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+
+// Fetching Records on Homepage
+
+$(document).ready(function(){
+
+    
+    // Fetch user inputs and return vet records 
+
+
+    $("#searchTopVets").submit(function(e){
+
+        e.preventDefault();
+
+        
+
+
+
+    }
+
+
+
+
+ }
+)
