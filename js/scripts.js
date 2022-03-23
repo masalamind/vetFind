7bbf963bb16b2cb3a806d1e9dd8333a6aad9acad
masalamind <themasalacollective@gmail.com>
2022-03-23 20:55:07 +0300

    Added ui logic to fetch values of search form

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,27 @@ $(document).ready(function(){
 
     
     // Fetch user inputs and return vet records 
+    $("#searchTopVets").submit(
+        function(e){
+
+            e.preventDefault();
+            
+            var animalCategory = $("#animalCategory").val();
+            var countyLocation = $("#countyLocation").val();
+
+            alert("hey ya");
+            console.log(animalCategory + " "+ countyLocation);
+
+        }
+
+    )
 
 
-    $("#searchTopVets").submit(function(e){
 
-        e.preventDefault();
 
-        
+
 
 
 
     }
-
-
-
-
- }
 )
